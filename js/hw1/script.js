b9952f63d794c5f8bc1ac8254e7fb8a858dbfc37
Anton <leybov.anton@gmail.com>
2021-01-02 19:32:58 +0200

    popups code refactoring

--- js/hw1/script.js
+++ js/hw1/script.js
@@ -3,21 +3,20 @@ const credentials = {
     password: 'qwerty',
 }
 
-let userDataInp = () => {
+let userSignIn = () => {
 
     let clearInp = () => {
         document.getElementById('userName').value = '';
         document.getElementById('userPass').value = '';            
     }
     
-    let popUpHoistingDone = (text) => {
+    let popUpHoisting = (text, background) => {
         
         let popUpDone = document.createElement('div');
         let popUpCross = document.createElement('div');
 
         popUpDone.classList.add('successPopUp');
-        popUpDone.classList.add('activeDone');
-        popUpDone.classList.remove('activeFail');
+        popUpDone.classList.add(background);
         popUpDone.innerHTML = text;
         popUpCross.classList.add('closeCross');
         popUpDone.appendChild(popUpCross);
@@ -28,56 +27,34 @@ let userDataInp = () => {
 
         document.body.appendChild(popUpDone);
 
-        setTimeout(
-            () => {
-                popUpDone.remove();
-            },
-            5000
-        )
-
+        // setTimeout(
+        //     () => {
+        //         popUpDone.remove();
+        //     },
+        //     5000
+        // )
     }
     
-    let popUpHoistingFail = (text) => {
-        let popUpDone = document.createElement('div');
-        let popUpCross = document.createElement('div');
-
-        popUpDone.classList.add('successPopUp');
-        popUpDone.classList.add('activeFail');
-        popUpDone.classList.remove('activeDone');
-        popUpDone.innerHTML = text;
-        popUpCross.classList.add('closeCross');
-        popUpDone.appendChild(popUpCross);
-
-        popUpCross.onclick = () => {
-            popUpDone.remove()
-        }
-
-        document.body.appendChild(popUpDone);
-
-        setTimeout(
-            () => {
-                popUpDone.remove();
-            },
-            5000
-        )
-    }
-
     let userName = document.getElementById('userName').value;
     let userPass = document.getElementById('userPass').value;
 
     if (userName == credentials.name && userPass == credentials.password) {
-        let text = 'Добро Пожаловать'
+        let text = `Добро Пожаловать <br> ${userName}`;
+        let background = 'activeDone';
         clearInp();
-        popUpHoistingDone(text);
+        popUpHoisting(text, background);
     } else if (userName == '') {
-        let text = 'Введите Логин'
-        popUpHoistingFail(text);
+        let text = 'Введите Логин';
+        let background = 'activeFail';
+        popUpHoisting(text, background);
     } else if (userPass == '') {
-        let text = 'Введите Пароль'
-        popUpHoistingFail(text);
+        let text = 'Введите Пароль';
+        let background = 'activeFail';
+        popUpHoisting(text, background);
     } else {
-        let text = 'Пользователь не найден'
-        popUpHoistingFail(text);
+        let text = `Пользователь ${userName} не найден`;
+        let background = 'activeFail';
+        popUpHoisting(text, background);
     }
     
 }
