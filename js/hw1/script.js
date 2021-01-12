f7dd899cbf4c86cdf07334df2ffa50c674998cdc
Anton <leybov.anton@gmail.com>
2021-01-12 23:41:54 +0200

    final commit

--- js/hw1/script.js
+++ js/hw1/script.js
@@ -1,76 +1,116 @@
 'use strict';
 
-const namesArr = [
-    'Капуста',
-    'Репа',
-    'Редиска',
-    'Морковка',
-]
-
-let namesStr = 'Вася;Петя;Вова;Олег';
-
-
 // task-1
-let joinNames = (...prop) => {
-    console.log(prop.join(' | '))
-}
+// const vegArr = 
+// [
+//     'Капуста',
+//     'Репа',
+//     'Редиска',
+//     'Морковка',
+// ]
+
+// let joinNames = (...prop) => {
+//     return prop.join(' | ')
+// }
+
+// const newArr = joinNames(...vegArr)
+// console.log(newArr);
+
+
+
+
 
 // task-2
-let addNamesArr = (names) => {
-    console.log(names.split(';'))
+let namesStr = 'Вася;Петя;Вова;Олег';
+
+let addNamesArr = (prop) => {
+    return prop.split(';')
 }
+const newNamesArr = addNamesArr(namesStr)
+console.log(newNamesArr)
+
+
+
 
 
 // task-3
 let sayHello = (name) => {
-    console.log(`Привет ${name}!`)
+    return `Привет ${name}!`
 }
 
+const sayHelloValue = sayHello('Усы Якубовича');
+console.log(sayHelloValue);
+
+
+
+
+
 // task-4
-
-// додумался еще таким, более "императивным" подходом :D. Не знаю, правильно ли использовать такое количество методов подряд. То, что массив сначала переобразуется к строке, потом обратно в массив - это норм?) Ниже через map сделал.
-// const fruitsToUpperCase = fruits.toString().toUpperCase().split(',');
-// console.log(fruitsToUpperCase)
-
+// не могу понять, почему ретерн работает только в однострочном выражении
 const fruits = ['яблоко', 'ананас', 'груша'];
 
 const fruitsToUpperCase = (prop) => {
-    prop.map(function (item) {
-        console.log(item.toUpperCase())
-    })
+    return prop.map(item => item.toUpperCase()) 
 }
+
+const upperFruits = fruitsToUpperCase(fruits);
+console.log(upperFruits)
+
+
+
+
 
 // task-5 
 const addOneForAll = (...prop) => {
-    prop.map(function (item) {
-        console.log(item += 1)
-    })
+    return prop.map((item) => (item += 1))
 }
+const valuePlusOne = addOneForAll(1, 2, 3, 4);
+console.log(valuePlusOne)
 
 
-// task 6, не понял как вернуть функции полученное значение :С Получается вывести сумму сохраненную в локальную переменную sum... Дальше мозг отказал)
+
+
+
+// task 6
 const getSum = (...prop) => {
-    let sum = 0;
-    prop.forEach((item) => sum += item)
-    console.log(sum)
+    let counter = 0;
+    prop.forEach((item) => counter += item);
+    return counter;
 }
+
+const sum = getSum(1, 2, 3, 4, 5, 6);
+console.log(sum);
+
+
+
+
 
 //task-7  
 const originArr = [1, 'hello', 2, 3, 4, '5', '6', 7, null];
 
-const newArr = (...prop) => {
-    return console.log(prop.filter(element => typeof (element) === 'number'))
+const getNumArr = (...prop) => {
+    return prop.filter(element => typeof (element) === 'number')
 }
+
+const newNumArr = getNumArr(...originArr);
+console.log(newNumArr)
+
+
+
+
 
 // task-8
 const valueArr = ['123', '', null, false, undefined, NaN, 3, 1, {}, ];
 
 let haveTrueValue = (...prop) => {
     let controlValue = 0;
-
     prop.map((item) => {
         (Boolean(item) === false) ? controlValue += 1: controlValue += 0
     });
 
-    (controlValue >= 1) ? console.log(`Количество ложных значений: ${controlValue} `): console.log('Все значение правдивы');
+    let out =  (controlValue >= 1) ? `Количество ложных значений: ${controlValue} ` : 'Все значение правдивы';
+    return out
 }
+
+const valuesCounter = haveTrueValue(...valueArr);
+console.log(valuesCounter)
